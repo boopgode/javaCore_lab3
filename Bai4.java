befc76e9a30a4a37f712ac51a63eaccff447147a
Gode <69381301+boopgode@users.noreply.github.com>
2024-01-09 21:00:26 +0700

    #02

--- Bai4.java
+++ Bai4.java
@@ -3,9 +3,13 @@ package com.devmaster.baitap;
 public class Bai4 {
     public static void main(String[] args) {
         Bai4 bai4 = new Bai4();
-        if(bai4.KTSNT(20)){
-            System.out.println("Là số nguyên tố");
-        }else System.out.println("Không phải số nguyên tố");
+//        if(bai4.KTSNT(20)){
+//            System.out.println("Là số nguyên tố");
+//        }else System.out.println("Không phải số nguyên tố");
+        System.out.println("dãy số nguyên tố :");
+        for (int i = 2; i < 100; i++) {
+            if (bai4.KTSNT(i)) System.out.printf("%d ", i);
+        }
     }
 
     boolean KTSNT(int a) {
