413bf895f0d5d1437a52affd5311e157287313f4
Gode <69381301+boopgode@users.noreply.github.com>
2024-01-09 19:23:06 +0700

    #01

---
+++ Bai1.java
@@ -0,0 +1,12 @@
+package com.devmaster.baitap;
+
+public class Bai1 {
+
+    public static void main(String[] args) {
+        int n=0;
+        for (int i=0;i<=10;i++){
+            n+=i;
+        }
+        System.out.println(n);
+    }
+}
