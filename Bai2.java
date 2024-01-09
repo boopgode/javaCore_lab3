2309aa7bc26bdf60d80b76f0260333106997b309
Gode <69381301+boopgode@users.noreply.github.com>
2024-01-09 19:38:54 +0700

    #01

--- Bai2.java
+++ Bai2.java
@@ -6,16 +6,37 @@ import java.util.Scanner;
 
 public class Bai2 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("nhập vào số n:");
-        int n = sc.nextInt();
-        int tong=0;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("nhập vào số n:");
+//        int n = sc.nextInt();
+//        int tong=0;
+//
+//        for (int i=0;i<=n;i++){
+//            tong+= i;
+//
+//        }
+//        System.out.println("tong "+tong);
+        Bai2 bai2= new Bai2();
+        // int
+//        int s= bai2.xuatTong2(10);
+//        System.out.println("tong từ 1 đến 10 = "+s);
 
-        for (int i=0;i<=n;i++){
-            tong+= i;
+        bai2.xuatTong2(100);// void
 
-        }
-        System.out.println("tong "+tong);
     }
+    void xuatTong2(int n){
+        int s=0;
+        for (int i=1;i<=n;i++){
+            s+=i;
+        }
+        System.out.println("tong từ 1 đến "+n+" = "+s);
+    }
+//     int xuatTong(int n){
+//        int s=0;
+//        for (int i=1;i<=n;i++){
+//            s+=i;
+//        }
+//        return s;
+//     }
 
 }
